57e4d435a86a5ede76c9cfd5250f1ba823b5e2b2
thandobhebhe <bukhosi95@gmail.com>
2021-05-11 12:55:44 +0100

    add web controller for potential rental movie

--- src/main/java/com/xtravision/Application/WebController.java
+++ src/main/java/com/xtravision/Application/WebController.java
@@ -1,25 +1,41 @@
 package com.xtravision.Application;
 
+import com.xtravision.movie.MoviePreOrder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class WebController {
 
     @RequestMapping("/home")
-    public String returnIndexPage(Model welcomModel){
+    public String returnIndexPage(Model welcomeModel){
 
-        welcomModel.addAttribute("welcomeMessage","Weclome to XVision");
+        welcomeModel.addAttribute("welcomeMessage","Welcome to XtraVision");
         return "index";
     }
 
     @RequestMapping("/rent")
-    public String returnRentPage(){
+    public String returnRentPage(Model rentalInstructorModel){
+
+        rentalInstructorModel.addAttribute("instruction", "Select a com.xtravision.movie and click rent");
         return "rent";
     }
 
+//    @RequestMapping(method = RequestMethod.POST, value = "/potentialRental")
+//    public String returnPotenialRentalPage(@RequestBody MoviePreOrder moviePreOrder){
+//
+//
+//
+//        return "potentialRental";
+//    }
+    @RequestMapping("/potentialRental")
+    public String returnPotenialRentalPage(){
 
+        return "potentialRental";
+    }
 
 
 }
